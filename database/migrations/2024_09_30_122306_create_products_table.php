924165271b1047779fb2b54c6547efef8a98875a
y104jm1013 <y104jk@icloud.com>
2024-09-30 22:03:17 +0900

    First commit

---
+++ database/migrations/2024_09_30_122306_create_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id')->notnull();
+            $table->integer('company_id')->notnull();
+            $table->string('product_name')->notnull();
+            $table->integer('price')->notnull();
+            $table->integer('stock')->notnull();
+            $table->text('comment')->nullable();
+            $table->string('img_path')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
